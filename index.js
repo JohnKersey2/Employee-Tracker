ddc432826164d81cf0f3ca78b1a1ab3a11e56d00
John Kersey <johnkersey2@yahoo.com>
2023-06-08 17:16:09 -0400

    Added remaining functions to navigate trackers.

--- index.js
+++ index.js
@@ -60,14 +60,153 @@ function init() {
     });
 }
 
-let viewAllDepartments = () => {
+viewAllDepartments = () => {
     db.query("SELECT * FROM department", (err, result) => {
       console.table(result);
     });
-    // init()
+    init()
   };
 
-  function exit() {
+  let viewAllRoles = () => {
+    db.query(
+      "SELECT department.*, role.*, role.id AS role_id FROM role INNER JOIN department on role.department_id = department.id",
+      (err, result) => {
+        console.table(result);
+      }
+    );
+  };
+  
+  let viewAllEmployees = () => {
+    db.query(
+      `SELECT e.id AS "Employee ID", 
+    e.first_name AS "First Name",
+    e.last_name AS "Last Name",
+    role.title AS "Role Title",
+    department.name AS "Department",
+    role.salary AS "Salary",
+    CONCAT(m.first_name, ' ', m.last_name) AS "Manager Name"
+    FROM employee e
+    LEFT JOIN role ON e.role_id = role.id
+    LEFT JOIN employee m ON m.id = e.manager_id
+    LEFT JOIN department ON role.department_id = department.id
+  `,
+      (err, result) => {
+        console.table(result);
+      }
+    );
+  };
+  
+addDepartment = async () => {
+    const departmentquestions = {
+      name: "name",
+      type: "input",
+      message: "enter department name",
+    };
+  
+    let answer = await inquirer.prompt(departmentquestions);
+    let departmentname = answer.name;
+    db.query(
+      "INSERT INTO department (name) VALUES (?)",
+      [departmentname],
+      (err, result) => {
+        console.log("department added");
+        console.log(err);
+      }
+    );
+  };
+  
+addRole = async () => {
+    const rolequestions = [
+      {
+        name: "role",
+        type: "input",
+        message: "enter role",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "enter salary",
+      },
+      {
+        name: "department",
+        type: "input",
+        message: "enter department_id",
+      },
+    ];
+  
+    let answer = await inquirer.prompt(rolequestions);
+    db.query(
+      "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)",
+      [answer.role, answer.salary, answer.department],
+      (err, result) => {
+        console.log("role added");
+      }
+    );
+  };
+  
+addEmployee = async () => {
+    const employeequestion = [
+      {
+        name: "firstname",
+        type: "input",
+        message: "enter first name",
+      },
+      {
+        name: "lastname",
+        type: "input",
+        message: "enter last name",
+      },
+      {
+        name: "role",
+        type: "input",
+        message: "enter role id",
+      },
+      {
+        name: "manager",
+        type: "input",
+        message: "enter manager id",
+      },
+    ];
+    let answer = await inquirer.prompt(employeequestion);
+    db.query(
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)",
+      [answer.firstname, answer.lastname, answer.role, answer.manager],
+      (err, result) => {
+        console.log("employee added");
+        console.log(err);
+      }
+    );
+  };
+  
+updateEmployeeRole = () => {
+    db.query("SELECT * FROM employee", async (err, result) => {
+      let employeeid = [];
+  
+      for (let i = 0; i < result.length; i++) {
+        employeeid.push(result[i].id);
+  
+      }
+      let question = [
+          {
+            name: "employeeid",
+            type: "list",
+            choices: employeeid,
+            message: "select employee id"
+          },
+          {
+              name: "role",
+              type: "input",
+              message: "enter role id"
+            }
+        ];
+        let answer = await inquirer.prompt(question)
+        db.query("UPDATE employee SET role_id = ? WHERE id = ?", [answer.role, answer.employeeid], (err, result) => {
+          console.log("employee role updated");
+        })
+    });
+  };
+
+function exit() {
     process.exit();
 }
 
