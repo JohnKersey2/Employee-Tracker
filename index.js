37961e551992df03f90bea70b7a8ef1e32a268ce
John Kersey <johnkersey2@yahoo.com>
2023-06-07 22:50:10 -0400

    Added inquirer prompt tree

--- index.js
+++ index.js
@@ -14,3 +14,50 @@ const db = mysql.createConnection(
     },
     console.log ('Connected to the employees_db database')
 );
+
+function init() {
+    inquirer.prompt ({
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: [
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update an Employee's Role",
+            "Exit"
+        ],
+    })
+    .then((answer) => {
+        switch (answer.menu) {
+            case "View All Departments":
+                viewAllDepartments();
+                break;
+            case "View All Roles":
+                viewAllRoles();
+                break;
+            case "View All Employees":
+                viewAllEmployees();
+                break;
+            case "Add a Department":
+                addDepartment();
+                break;
+            case "Add a Role":
+                addRole();
+                break;
+            case "Add an Employee":
+                addEmployee();
+                break;
+            case "Update an Employee's Role":
+                updateEmployeeRole();
+                break;
+            case "Exit":
+                exit();
+        }
+    });
+}
+
+init()
