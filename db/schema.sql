0202d1f0417a8a7d62ba4011d9104b1968394a7d
John Kersey <johnkersey2@yahoo.com>
2023-06-07 20:53:43 -0400

    Added schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,29 @@
-DROP DATABASE IF EXISTS employeeInfo_db;
-CREATE DATABASE employeeInfo_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE employeeInfo_db;
+USE employees_db
 
-CREATE TABLE departments(
-    id: INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name: VARCHAR(30 NOT NULL)
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
 );
 
-CREATE TABLE roles(
-    id: INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    title: VARCHAR(30) NOT NULL,
-    salary: DECIMAL NOT NULL,
-    department_id: INT
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
-CREATE TABLE employees(
-    id: INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT,
-    manager_id: INT
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY(manager_id) REFERENCES employees(id)
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const mysql = require("mysql2")
+const inquirer = require('inquirer')
+const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "process.env.PASSWORD",
-    database: "employee_db",
-  });
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'employees_db',
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PW,
+        // database: process.env.DB_NAME
+    },
+    console.log ('Connected to the employees_db database')
+);
